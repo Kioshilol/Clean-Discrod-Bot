64b9b4bbe4a8e29db6715815a7fd56e69b6ceb76
Pavel <48092890+Kioshilol@users.noreply.github.com>
2021-10-27 01:56:58 +0300

    Add reaction

--- SmartDiscordBot/SmartDiscordBot/Bot/Services/CommandHandler.cs
+++ SmartDiscordBot/SmartDiscordBot/Bot/Services/CommandHandler.cs
@@ -16,28 +16,61 @@ namespace SmartDiscordBot.Bot.Services
         public readonly IConfiguration _configuration;
         public readonly CommandService _commandService;
 
-        public CommandHandler(IServiceProvider provider, IConfiguration configuration, ILogger<CommandHandler> logger, DiscordSocketClient client, CommandService commands) : base(client, logger)
+        public CommandHandler(IServiceProvider provider, IConfiguration configuration, ILogger<CommandHandler> logger, DiscordSocketClient client, CommandService commandService) : base(client, logger)
         {
             _provider = provider;
             _configuration = configuration;
-            _commandService = commands;
+            _commandService = commandService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             Client.MessageReceived += OnMessageReceived;
+            Client.JoinedGuild += OnJoinedGuild;
+            Client.ReactionAdded += OnReactionAdded;
             _commandService.CommandExecuted += OnCommandExecute;
+
             await _commandService.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
         }
 
-        private Task OnCommandExecute(Discord.Optional<CommandInfo> arg1, ICommandContext arg2, IResult arg3)
+        private async Task OnReactionAdded(Discord.Cacheable<Discord.IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
-            return Task.CompletedTask;
+            if(arg3.Message.IsSpecified)
+            {
+                if (arg3.Message.Value.Author.Id == Client.CurrentUser.Id)
+                {
+                    await arg3.Channel.SendMessageAsync(Constants.ThxForReaction);
+                }
+            }
+            
         }
 
-        private Task OnMessageReceived(SocketMessage arg)
+        private async Task OnJoinedGuild(SocketGuild arg)
         {
-            return Task.CompletedTask;
+            await arg.DefaultChannel.SendMessageAsync(Constants.Greetings);
+        }
+
+        private async Task OnMessageReceived(SocketMessage arg)
+        {
+            if (!(arg is SocketUserMessage message))
+                return;
+
+            if (message.Source != Discord.MessageSource.User)
+                return;
+
+            var argsPos = 0;
+
+            if (!message.HasStringPrefix(_configuration["prefix"], ref argsPos) && !message.HasMentionPrefix(Client.CurrentUser, ref argsPos))
+                return;
+
+            var context = new SocketCommandContext(Client, message);
+            await _commandService.ExecuteAsync(context, argsPos, _provider);
+        }
+
+        private async Task OnCommandExecute(Discord.Optional<CommandInfo> arg1, ICommandContext arg2, IResult arg3)
+        {
+            if (!arg1.IsSpecified || !arg3.IsSuccess)
+                await arg2.Channel.SendMessageAsync($"Error: {arg3}");
         }
     }
 }
--- SmartDiscordBot/SmartDiscordBot/Constants.cs
+++ SmartDiscordBot/SmartDiscordBot/Constants.cs
@@ -9,5 +9,12 @@ namespace SmartDiscordBot
         public const string RedditGetMemoUrl = "https://reddit.com/r/memes/random.json?limit=1";
         public const string ManualBotConfig = "_manualconfig.yml";
         public const string BotConfig = "botsettings.json";
+
+        #region Text
+
+        public const string Greetings = "Naruto, Let’s show them our power.";
+        public const string ThxForReaction = "Thanks for reaction bro!";
+
+        #endregion
     }
 }
